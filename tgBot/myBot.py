78403d448ccbef30332d2ad4b67ce3b8ebaeab1d
vaka <ivanka6342@gmail.com>
2018-11-21 19:19:23 +0300

    work was corrected

--- tgBot/myBot.py
+++ tgBot/myBot.py
@@ -5,13 +5,10 @@ import requests
 import json
 import botToken
 
-token = botToken.token
-requestLine = 'https://api.telegram.org/bot' + token + '/'
+requestLine = 'https://api.telegram.org/bot' + botToken.token + '/'
 
 def init_bot():
-	with open('token.txt', 'r') as tokenFile:
-		botToken = tokenFile.read()
-	return telepot.Bot(botToken)
+	return telepot.Bot(botToken.token)
 
 def get_updates():
 	req = requestLine + 'getUpdates'
@@ -19,21 +16,20 @@ def get_updates():
 	return result.json()
 
 def get_message():
-	data = get_updates
-	chat_id = data['result'][-1]['message']['chat']['id']
-	msg_text = data['result'][-1]['message']['text']
-	message = { 'chat_id' : chat_id, 
-				'message_text' : msg_text }
+	data = get_updates()
+	chat_id     = data['result'][-1]['message']['chat']['id']
+	sender_name = data['result'][-1]['message']['from']['username']
+	message = { 'chat_id'      : chat_id,
+				'sender_name'  : sender_name }
 	return message
 
-def send_message(chat_id, msg_text='dafault text'):
-	req = requestLine + 'sendMessage?chat_id={}&text={}'.format(chat_id, msg_text)
+def send_message(chat_id, sender_name):
+	req = requestLine + 'sendMessage?chat_id={}&text={}'.format(chat_id, sender_name + ', U R 64Y')
 	requests.get(req)
 
 def main():
-	answer = get_message()
-	chat_id = answer['chat_id']
-	send_message(chat_id)
+	last_msg = get_message()
+	send_message(last_msg['chat_id'], last_msg['sender_name'])
 	#d = getUpdates()
 	#with open('dumpJSONrequest.txt', 'w') as f:
 	#	json.dump(d, f, indent = 2, ensure_ascii = False)
